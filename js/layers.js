3476199ddd91fc1b77455ee2118eacd36f8b9812
qwerty2281444 <alihanryspek044@gmail.com>
2025-06-07 22:51:42 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -68,7 +68,7 @@ addLayer("w", {
 })
 
 addLayer("s", {
-    name: "wings", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "snakes", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
